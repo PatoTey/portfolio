1fd3cdaf263c88e04e791c5e7f611d87785c8b9f
Teyo <73722247+PatoTey@users.noreply.github.com>
2023-08-29 16:59:00 -0400

    modified data and images for gamon projet

--- src/data/PagesData.js
+++ src/data/PagesData.js
@@ -1,11 +1,24 @@
 import csmob from "../images/csmobileshot.png"
 import csdesk from "../images/csdeskshot.png"
 import mclmob from "../images/mclmob.png"
-import gmmob from "../images/gmmob2.png"
+import gmmob from "../images/gmmob.png"
+import gmdesk from "../images/gmdesk.png"
 
 const data = [
     {
         id: 1,
+        appname: "Gamon",
+        description: "A game marketplace, where you can find the game you want! Here you can view and order your games",
+        madewith: "React, React-Router, Node Express, MySQL, PayPal API",
+        imagedesk: gmdesk,
+        imagemob: gmmob,
+        hasdemo: true,
+        appurl: "https://gamon.netlify.app",
+        hascode: true,
+        githuburl: "https://github.com/PatoTey/Gamon"
+    },
+    {
+        id: 2,
         appname: "CoffeStop",
         description: "A web for ordering your favorite type of coffe!",
         madewith: "React, React-Router, Firebase",
@@ -15,18 +28,6 @@ const data = [
         appurl: "https://coffeestopshop.netlify.app",
         hascode: true,
         githuburl: "https://github.com/PatoTey/CoffeeStop"
-    },
-    {
-        id: 2,
-        appname: "Gamon",
-        description: "A game store app",
-        madewith: "React, React-Router, Node Express, MySQL",
-        imagedesk: undefined,
-        imagemob: gmmob,
-        hasdemo: false,
-        appurl: "",
-        hascode: false,
-        githuburl: ""
     },
     {
         id: 3,
